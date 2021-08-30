052797f1e4ade67f5b033efc91dbc22d6287ee2e
Cdt226 <67614046+Cdt226@users.noreply.github.com>
2021-08-30 10:40:46 +0000

    Add files via upload

---
+++ hilla_script.js
@@ -0,0 +1,20 @@
+function dropMenu()
+{
+	document.getElementById("header-dropdown").style.display ="all";
+	document.getElementById("header-dropdown").style.marginLeft ="-140px";
+	document.getElementById("header-dropdown").style.transition = "0.3s";
+}
+
+
+
+
+/*list-style:none;
+	display: all;
+  	position: absolute;
+  	margin-top: 0px;
+ 	width: 200px;
+  	background-color: #ffff;#f9f9f9;
+  	z-index: 1;
+  	border-radius:3px;
+  	border:1px solid #4d0000;
+  	margin-left: 100px;*/
